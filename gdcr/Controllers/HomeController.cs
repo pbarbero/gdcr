760ef0e0142b1f44259730098bcf6a657725b9d3
pbarbero <pbarberoiriarte@gmail.com>
2018-11-09 19:26:56 +0100

    Add GameOfLife project reference and start using it

--- gdcr/Controllers/HomeController.cs
+++ gdcr/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using gdcr.Models;
+using GameOfLife.Library;
 
 namespace gdcr.Controllers
 {
@@ -15,29 +16,34 @@ namespace gdcr.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult GameOfLife()
         {
             ViewData["Message"] = "Your application description page.";
 
-            return View();
-        }
+            var result = PlayGame();
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return View(result);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private Dictionary<int, List<Cell>> PlayGame()
+        {
+            var result = new Dictionary<int, List<Cell>>();
+            var grid = new Grid(100, 100);
+            grid.SetCenterSeed(10, 100, 100);
+
+            for (int i = 0; i < 2; i++)
+            {
+                grid.NextIteration();
+                result.Add(i, grid.Cells);
+            }
+
+            return result;
         }
     }
 }
