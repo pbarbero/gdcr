515acb39c928d09f550ad90a2240ae5f9f92f885
Pilar Barbero Iriarte <pilar.barbero@col.vueling.com>
2018-11-09 13:54:30 +0100

    Create asp net core project + sln

---
+++ gdcr/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gdcr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
